8b275feec16dc6da8cf7e1bfcbe1fc79992f95ce
Carin Y Gardner <cariny.gardner@gmail.com>
2025-01-11 20:47:31 -0800

    Completed exercise 02

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What was your grade percentage? ");
+        string userInput = Console.ReadLine();
+        float gradeReceived = float.Parse(userInput);
+
+        string letter = "";
+
+        if (gradeReceived >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradeReceived >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradeReceived >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradeReceived >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (gradeReceived >= 70)
+        {
+            Console.WriteLine("You passed! Good job!");
+        }
+        else
+        {
+            Console.WriteLine("Not quite there yet. Keep studying! Better luck next time!");
+        }
     }
 }
