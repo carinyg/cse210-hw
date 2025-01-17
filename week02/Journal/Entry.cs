85383265a4aadf996d27d7d1033121e2906512af
Carin Y Gardner <cariny.gardner@gmail.com>
2025-01-16 21:08:20 -0800

    coded Entry.Display

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -6,7 +6,9 @@ public class Entry
 
     public void Display()
     {
-        
+        Console.WriteLine(_date);
+        Console.WriteLine(_promptText);
+        Console.WriteLine(_entryText);
     }
 
 }
