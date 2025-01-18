a08714ed101c0e0ff47ce91a1856c43f34f09b6b
Carin Y Gardner <cariny.gardner@gmail.com>
2025-01-17 21:11:50 -0800

    working top level loop

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        while (true)
+        {
+            Console.WriteLine("1. Create new entry");
+            Console.WriteLine("2. Display All Entries");
+            Console.WriteLine("3. Save to File");
+            Console.WriteLine("4. Load from File");
+            Console.WriteLine("5. Quit");
+
+            Console.WriteLine("Enter your selection: ");
+
+            string userChoice = Console.ReadLine();
+
+            Console.WriteLine($"You chose {userChoice}");
+        }
     }
 }
