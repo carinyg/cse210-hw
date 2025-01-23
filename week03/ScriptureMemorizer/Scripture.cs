78c29694917db61ba6df912072c22f4b1fb40a75
Carin Y Gardner <cariny.gardner@gmail.com>
2025-01-22 20:43:23 -0800

    stubbed out program design

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,25 @@
+public class Scripture
+{
+    public string _reference;
+    public List<Word> _words;
+
+    public Scripture(string Reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+}
---
+++ week03/ScriptureMemorizer/Word.cs
@@ -0,0 +1,30 @@
+public class Word
+{
+    public string _text;
+    public bool _isHidden;
+
+    public Word(string text)
+    {
+
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+
+    }
+
+    public bool IsHidden()
+    {
+        return false;
+    }
+
+    public string GetDisplayText()
+    {
+        return "";
+    }
+}
