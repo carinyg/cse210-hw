19873b244a6fdcd8b2a3049e8a50d8a3bbd26795
Carin Y Gardner <cariny.gardner@gmail.com>
2025-01-28 22:03:12 -0800

    thought I was done, but it's not working correctly

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
 
 class Program
 {
@@ -6,12 +8,31 @@ class Program
     {
         Reference r1 = new Reference("James", 1, 5, 6);
 
-        string scriptureText = "If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him. But let him ask in faith, nothing wavering. For he that wavereth is like a wave of the sea driven with the wind and tossed.";
-        Scripture s1 = new Scripture(r1, scriptureText);
-
+        Scripture s1 = new Scripture(r1,"If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him. But let him ask in faith, nothing wavering. For he that wavereth is like a wave of the sea driven with the wind and tossed.");
         
+        bool hidden = s1.IsCompletelyHidden();
+        string input = "";  
+        
+        while ((input != "quit") || (hidden == false))
+        {
+            Console.Clear();
+            string reference = r1.GetDisplayText();
+            string scripture = s1.GetDisplayText();
+            Console.WriteLine($"{reference} {scripture}");
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            input = Console.ReadLine();
+            s1.HideRandomWords(3);
+            hidden = s1.IsCompletelyHidden();
+        }
 
+        if (hidden == true)
+        {
+            Console.WriteLine("Congratulations! You have successfully memorized the scripture!");
+        }
 
-        Console.Write(r1.GetDisplayText());
+        if (input == "quit")
+        {
+            Console.WriteLine("Goodbye!");
+        }   
     }
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,32 +1,75 @@
+using System;
+using System.Collections.Generic;
+
 public class Scripture
 {
     private Reference _reference;
-    private List<Word> _words;
+    private List<Word> _words = new List<Word>();
 
-    public Scripture(string Reference, string text)
+    public Scripture(Reference Reference, string text)
     {
-        _reference = new Reference(Reference);
+        _reference = Reference;
         
-        string[] text = scriptureText.Split(' ');
-        foreach (string word in scriptureWords)
+        string[] splitText = text.Split(' ');
+
+        foreach (string word in splitText)
         {
             Word w = new Word(word);
+            _words.Add(w);
         }
 
     }
 
     public void HideRandomWords(int numberToHide)
     {
+        bool test;
+
+        do{
+            Random rand = new Random();
+            int randomIndex = rand.Next(0, _words.Count);
+
+            int repeat = numberToHide;
+
+            do{
+                if (_words[randomIndex].IsHidden() == false)
+                {
+                    _words[randomIndex].Hide();
+                    repeat--;
+                }
+                else
+                {
+                    randomIndex = rand.Next(0, _words.Count);
+                }
+
+                test = IsCompletelyHidden();
+            }while(repeat > 0);
+
+        }while(test == true);
 
     }
 
     public string GetDisplayText()
     {
-        return "";
+        string displayText = "";
+        foreach (Word w in _words)
+        {
+            displayText += w.GetDisplayText() + " ";
+        }
+        return displayText;
     }
 
     public bool IsCompletelyHidden()
     {
-        return false;
+        bool fullyHidden = true;
+
+        foreach (Word w in _words)
+        {
+            if (w.IsHidden() == false)
+            {
+                fullyHidden = false;
+            }
+        }
+
+        return fullyHidden;
     }
 }
