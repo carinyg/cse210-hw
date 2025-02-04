91b2a6db2b4a4d484e70d0d8d4f715d2a4fc87f7
Carin Y Gardner <cariny.gardner@gmail.com>
2025-02-03 21:24:59 -0800

    Assignment Complete

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -26,10 +26,29 @@ class Program
         vid2.AddComment(com7);
         videos.Add(vid2);
 
-        
+        Video vid3 = new Video("TRADE WAR and Naked Grammys!", "Ben Shapiro", 50);
+        Comment com8 = new Comment("@magwitch1712", "Congratulations Ben on winning Spotify award as #1 Conservative Podcaster with 100 million streams. Well done, you deserve it");
+        vid3.AddComment(com8);
+        Comment com9 = new Comment("@billm5555", "I love that Marco speaks fluent Spanish. That is going to be invaluable in this region");
+        vid3.AddComment(com9);
+        Comment com10 = new Comment("@thehunzz", "South Africa isn't a failing state, it's a failed state.");
+        vid3.AddComment(com10);
+        videos.Add(vid3);
 
+        Video vid4 = new Video("Tariff Threats Get Results, and Absurdity at Grammys and DNC...", "Megyn Kelly", 110);
+        Comment com11 = new Comment("@darrenmiller6927", "Keep your NBC, ABC, CNN, etc. We want Megyn! Top news show in the country!");
+        vid4.AddComment(com11);
+        Comment com12 = new Comment("@redphrmgrl2457", "I love Rick Grenell. Thank you for getting them home!");
+        vid4.AddComment(com12);
+        Comment com13 = new Comment("@barbaraponton8350", "This is why legacy media is dead, because we are watching Megan for the truth!!");
+        vid4.AddComment(com13);
+        Comment com14 = new Comment("@alejandraredmond2362", "Mr Grenell is an impressive diplomat!");
+        vid4.AddComment(com14);
+        videos.Add(vid4);
 
-
-
+        foreach (Video video in videos)
+        {
+            Console.WriteLine(video.GetDisplayVideoAndComments());
+        }
     }
 }
