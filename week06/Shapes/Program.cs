49bc123338ad77376f5c66917e18700a3f1a42f7
Carin Y Gardner <cariny.gardner@gmail.com>
2025-02-11 21:34:20 -0800

    created and tested Rectangle class

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -7,5 +7,10 @@ class Program
         Shape shape = new Square("blue", 14);
         Console.WriteLine($"Area of the square is {shape.GetArea()}");
         Console.WriteLine($"Color of the square is {shape.GetColor()}");
+
+        shape = new Rectangle("red", 10, 20);
+        Console.WriteLine($"Area of the rectangle is {shape.GetArea()}");
+        Console.WriteLine($"Color of the rectangle is {shape.GetColor()}");
+        
     }
 }
