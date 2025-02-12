86e2499b245c95de2f53464a5c431dfba02823c4
Carin Y Gardner <cariny.gardner@gmail.com>
2025-02-11 21:43:04 -0800

    Built and tested list

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,16 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Shape shape = new Square("blue", 14);
-        Console.WriteLine($"Area of the square is {shape.GetArea()}");
-        Console.WriteLine($"Color of the square is {shape.GetColor()}");
+        // Shape shape = new Square("blue", 14);
+        // Console.WriteLine($"Area of the square is {shape.GetArea()}");
+        // Console.WriteLine($"Color of the square is {shape.GetColor()}");
 
-        shape = new Rectangle("red", 10, 20);
-        Console.WriteLine($"Area of the rectangle is {shape.GetArea()}");
-        Console.WriteLine($"Color of the rectangle is {shape.GetColor()}");
+        // shape = new Rectangle("red", 10, 20);
+        // Console.WriteLine($"Area of the rectangle is {shape.GetArea()}");
+        // Console.WriteLine($"Color of the rectangle is {shape.GetColor()}");
 
-        shape = new Circle("yellow", 7);
-        Console.WriteLine($"Area of the circle is {shape.GetArea()}");
-        Console.WriteLine($"Color of the circle is {shape.GetColor()}");
+        // shape = new Circle("yellow", 7);
+        // Console.WriteLine($"Area of the circle is {shape.GetArea()}");
+        // Console.WriteLine($"Color of the circle is {shape.GetColor()}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("blue", 14));
+        shapes.Add(new Rectangle("red", 10, 20));
+        shapes.Add(new Circle("yellow", 7));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Area of the shape is {shape.GetArea()}");
+            Console.WriteLine($"Color of the shape is {shape.GetColor()}");
+        }
     }
 }
