8f813d8a7164c1d5a9db8e1ee59dc50ea925d182
Carin Y Gardner <cariny.gardner@gmail.com>
2025-02-12 21:21:59 -0800

    GoalManager stubbed out

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,32 @@
+public class GoalManager
+{
+    private List<GoalManager> _goals;
+    private int _score;
+
+    public GoalManager()
+    {}
+
+    public void Start()
+    {}
+
+    public void DisplayPlayerInfo()
+    {}
+
+    public void ListGoalNames()
+    {}
+
+    public void ListGoalDetails()
+    {}
+
+    public void CreateGoal()
+    {}
+
+    public void RecordEvent()
+    {}
+
+    public void SaveGoals()
+    {}
+
+    public void LoadGoals()
+    {}
+}
