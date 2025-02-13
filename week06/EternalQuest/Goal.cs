fe7e4c084edd1bb21879dd06192cd5411e78d00f
Carin Y Gardner <cariny.gardner@gmail.com>
2025-02-12 21:39:05 -0800

    stubbed out Goals and SimpleGoals

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,25 @@
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private string _points;
+
+    public Goal(string name, string description, string points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+
+    public abstract void RecordEvent();
+    
+
+    public abstract bool IsComplete();
+
+    public string GetDetailsString()
+    {
+        return "";
+    }
+
+    public abstract string GetStringRepresentation();
+}
