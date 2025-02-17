155ac2ec26240d4dd762222400d9814d49742e29
Carin Y Gardner <cariny.gardner@gmail.com>
2025-02-17 15:05:52 -0800

    Coded Main

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.Clear();
+        List<Activity> _activities = new List<Activity>();
+        _activities.Add(new Running("17 Feb 2025", 30, 3.0f));
+        _activities.Add(new Cycling("17 Feb 2025", 45, 20));
+        _activities.Add(new Swimming("17 Feb 2025", 60, 20));
+
+
+        foreach (Activity activity in _activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+            Console.WriteLine();
+        }
     }
 }
