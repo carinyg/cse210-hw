2568a6bd54f96eb032e14dde9f2e98e34ff7d70d
Carin Y Gardner <cariny.gardner@gmail.com>
2025-03-03 19:12:45 -0800

    finalize course

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -10,7 +10,6 @@ class Program
         _activities.Add(new Cycling("17 Feb 2025", 45, 20));
         _activities.Add(new Swimming("17 Feb 2025", 60, 20));
 
-
         foreach (Activity activity in _activities)
         {
             Console.WriteLine(activity.GetSummary());
